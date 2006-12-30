299571aeb2daeb0879a9c37ebf819c06be32e216
ricmac <ricmac@8d40a0d9-8522-0410-86c4-29b74ebb93e8>
2006-12-30 01:24:45 +0000

    GNU-GPL licensing

--- cuencas/src/hydroScalingAPI/mainGUI/ParentGUI.java
+++ cuencas/src/hydroScalingAPI/mainGUI/ParentGUI.java
@@ -1,4 +1,24 @@
 /*
+CUENCAS is a River Network Oriented GIS
+Copyright (C) 2005  Ricardo Mantilla
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+/*
  * parentGUI.java
  *
  * Created on March 3, 2003, 4:31 PM
@@ -1178,8 +1198,8 @@ public class ParentGUI extends javax.swing.JFrame implements javax.swing.event.I
         help.add(jSeparator10);
 
         license.setFont(new java.awt.Font("Verdana", 0, 10));
-        license.setMnemonic('u');
-        license.setText("Update");
+        license.setMnemonic('l');
+        license.setText("License");
         license.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 licenseActionPerformed(evt);
