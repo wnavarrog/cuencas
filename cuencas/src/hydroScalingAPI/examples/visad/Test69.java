0d4447c80242e7a6b2cebeeb7b72ade034d58165
furey <furey@8d40a0d9-8522-0410-86c4-29b74ebb93e8>
2007-03-21 20:37:52 +0000

    ""

--- cuencas/src/hydroScalingAPI/examples/visad/Test69.java
+++ cuencas/src/hydroScalingAPI/examples/visad/Test69.java
@@ -84,7 +84,7 @@ public class Test69
                                    "A B C D E F G H I J K L M",
                                    "NOPQRSTUVWXYZ",
                                    "0123456789  - + = / [ ] ( ) { }",
-                                   "á é í ó ú ñ Á É Í Ó Ú Ñ"};
+                                   "p p p p p p p p p p p p"};
     if (sphere) {
       names = new String[] {"", "", "a b c d e f g h i j k l m n o p q " +
                             "r s t u v w x y z A B C D E F G H I J K L " +
