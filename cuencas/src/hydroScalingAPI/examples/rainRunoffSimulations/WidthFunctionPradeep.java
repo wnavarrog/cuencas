26ace4638f3566da15e486d87878af2434b6241f
pradeep.mandapaka <pradeep.mandapaka@8d40a0d9-8522-0410-86c4-29b74ebb93e8>
2009-04-07 21:03:37 +0000

    committing the changes made in the paths to run the program on Teragrid cluster

--- cuencas/src/hydroScalingAPI/examples/rainRunoffSimulations/WidthFunctionPradeep.java
+++ cuencas/src/hydroScalingAPI/examples/rainRunoffSimulations/WidthFunctionPradeep.java
@@ -80,9 +80,9 @@ public class WidthFunctionPradeep extends java.lang.Object {
     public static void main(String args[]) {
         
         try{ 
-        java.io.File theFile=new java.io.File("C:/Documents and Settings/pmandapa/My Documents/CuencasDatabases/Whitewater_database/Rasters/Topography/1_ArcSec_USGS_2005/Whitewaters.metaDEM");
+        java.io.File theFile=new java.io.File("~/CuencasDatabases/Whitewater_database/Rasters/Topography/1_ArcSec_USGS_2005/Whitewaters.metaDEM");
         hydroScalingAPI.io.MetaRaster metaModif=new hydroScalingAPI.io.MetaRaster(theFile);
-        metaModif.setLocationBinaryFile(new java.io.File("C:/Documents and Settings/pmandapa/My Documents/CuencasDatabases/Whitewater_database/Rasters/Topography/1_ArcSec_USGS_2005/Whitewaters.dir"));
+        metaModif.setLocationBinaryFile(new java.io.File("~/CuencasDatabases/Whitewater_database/Rasters/Topography/1_ArcSec_USGS_2005/Whitewaters.dir"));
         
         metaModif.setFormat("Byte");
         byte [][] matDirs=new hydroScalingAPI.io.DataRaster(metaModif).getByte();
@@ -91,7 +91,7 @@ public class WidthFunctionPradeep extends java.lang.Object {
         metaModif.setFormat("Integer");
         int [][] magnitudes=new hydroScalingAPI.io.DataRaster(metaModif).getInt();
        
-        new WidthFunctionPradeep(1063,496,matDirs,magnitudes,metaModif,new java.io.File("E:/Documents and Settings/pmandapa/My Documents/Research/Cuencas"));
+        new WidthFunctionPradeep(1063,496,matDirs,magnitudes,metaModif,new java.io.File("~/Results/Cuencas"));
         } catch (java.io.IOException IOE){
             System.out.print(IOE);
             System.exit(0);
