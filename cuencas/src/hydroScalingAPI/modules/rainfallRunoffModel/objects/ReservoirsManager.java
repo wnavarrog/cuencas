af5eb1487638b70a9e5899b4e71bf2be63c9e19d
tibebu.bekele <tibebu.bekele@8d40a0d9-8522-0410-86c4-29b74ebb93e8>
2011-11-09 20:47:42 +0000

    Initial Changes related to distributed reservoirs in the River Network by Tibebu

--- cuencas/src/hydroScalingAPI/modules/rainfallRunoffModel/objects/ReservoirsManager.java
+++ cuencas/src/hydroScalingAPI/modules/rainfallRunoffModel/objects/ReservoirsManager.java
@@ -9,16 +9,57 @@ package hydroScalingAPI.modules.rainfallRunoffModel.objects;
  * @author ricardo
  */
 public class ReservoirsManager {
-
+    
+    int[] resLocations;
+    double[] maxStorages;
+    double[] maxDepths;
+    double[] weirLengths;
+    double[] orficeDiameter;
+    
+    
     public ReservoirsManager() {
+        resLocations = new int[]{16};//{16,34};
+        maxStorages=new double[] {466663,466663};
+        maxDepths = new double[] {7, 7};
+        weirLengths = new double[] {2, 2};
+        orficeDiameter = new double[] {.6, .6};
+        
     }
     
     public int[] getReservoirLocations(){
-        return new int[] {16,34};
+        return resLocations;
     }
     
-    public float[] getReservoirMaxStorages(){
-        return new float[] {1000,1000};
+    public double[] getReservoirMaxStorages(){
+        return maxStorages;
+    }
+    
+    public double getReservoirYield(int i,double storage, double NaturalYield){
+        
+        double yield;
+        double deltaT = 1;
+        
+        double h = maxDepths[i]*Math.pow(storage/maxStorages[i],0.26);
+        if(h<maxDepths[i])
+        {
+            yield = (deltaT*0.6*0.25*Math.PI*Math.pow(orficeDiameter[i], 2)*Math.sqrt(2*9.81*h));
+        }
+        else
+        {
+            yield = (deltaT*0.6*0.25*Math.PI*Math.pow(orficeDiameter[i], 2)*Math.sqrt(2*9.81*h)+deltaT*3.1*weirLengths[i]*Math.pow(h-maxDepths[i], 1.5));
+        }
+        
+//        yield = 0; 
+        
+        
+//        System.out.println("Comparing Flows: Yield ="+yield+" Natural Flow = "+NaturalYield+ "For H="+h);
+        
+        
+        if(yield > NaturalYield) yield = NaturalYield;
+        
+        
+        
+        return yield;
     }
 
     /**
